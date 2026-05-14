6844540c0bb07fe44f345e637af91992f6e87614
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-05-14 11:08:11 -0500

    feat(shared): add open api configuration for documentation with swagger.

---
+++ src/main/java/com/resolum/intiva/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,39 @@
+package com.resolum.intiva.platform.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @Value("${documentation.application.description}")
+    String applicationDescription;
+
+    @Value("${documentation.application.version}")
+    String applicationVersion;
+
+    @Bean
+    public OpenAPI intivaPlatformOpenApi() {
+        // General configuration for OpenAPI
+        var openApi = new OpenAPI();
+        openApi.info(new Info()
+                        .title(this.applicationName)
+                        .description(this.applicationDescription)
+                        .version(this.applicationVersion)
+                        .license(new License().name("Apache 2.0")
+                                .url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Intiva Platform Wiki Documentation")
+                        .url("https://intiva-platform.wiki.github.io/docs"));
+
+        return openApi;
+    }
+}
