e973bb37fc30af839a1525adc7c1e20dd2442ce5
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-05-14 13:48:42 -0500

    feat(sign-up): add resources and assemblers.

--- src/main/java/com/resolum/intiva/platform/iam/domain/model/commands/SignUpCommand.java
+++ src/main/java/com/resolum/intiva/platform/iam/domain/model/commands/SignUpCommand.java
@@ -1,6 +1,5 @@
 package com.resolum.intiva.platform.iam.domain.model.commands;
 
-import com.resolum.intiva.platform.iam.domain.model.entities.Role;
 import com.resolum.intiva.platform.iam.domain.model.valueobjects.Email;
 
 /**
@@ -8,7 +7,6 @@ import com.resolum.intiva.platform.iam.domain.model.valueobjects.Email;
  *
  * @param email the email of the user to sign up
  * @param password the password of the user to sign up
- * @param role the role of the user to sign up
  */
-public record SignUpCommand(Email email, String password, Role role) {
+public record SignUpCommand(Email email, String password) {
 }
---
+++ src/main/java/com/resolum/intiva/platform/iam/interfaces/rest/resources/responses/UserResource.java
@@ -0,0 +1,9 @@
+package com.resolum.intiva.platform.iam.interfaces.rest.resources.responses;
+
+/**
+ * UserResource is a record that represents a user resource in the REST API. It contains the user's id and email.
+ * @param id the user's id
+ * @param email the user's email
+ */
+public record UserResource(String id, String email) {
+}
