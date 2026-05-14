267b1aab7e22890202f990f9c89a15f627b1bfc7
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-05-14 12:51:30 -0500

    feat(sign-up): add interfaces for hashing and token service.

---
+++ src/main/java/com/resolum/intiva/platform/iam/application/internal/outboundservices/TokenService.java
@@ -0,0 +1,28 @@
+package com.resolum.intiva.platform.iam.application.internal.outboundservices;
+
+/**
+ * Service interface for handling token generation, extraction, and validation.
+ */
+public interface TokenService {
+
+    /**
+     * Generate a token for a given username
+     * @param username the username
+     * @return String the token
+     */
+    String generateToken(String username);
+
+    /**
+     * Extract the username from a token
+     * @param token the token
+     * @return String the username
+     */
+    String getUsernameFromToken(String token);
+
+    /**
+     * Validate a token
+     * @param token the token
+     * @return boolean true if the token is valid, false otherwise
+     */
+    boolean validateToken(String token);
+}
