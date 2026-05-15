923fccb0a29975e6f98aed5391e549a1169ca717
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-05-14 19:01:37 -0500

    feat(transactions): add transaction jpa repository.

---
+++ src/main/java/com/resolum/intiva/platform/finances/interfaces/persistence/jpa/repositories/TransactionRepository.java
@@ -0,0 +1,31 @@
+package com.resolum.intiva.platform.finances.interfaces.persistence.jpa.repositories;
+
+import com.resolum.intiva.platform.finances.domain.model.aggregates.Transaction;
+import com.resolum.intiva.platform.finances.domain.model.valueobjects.TransactionTypes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Repository interface for managing Transaction entities in the database.
+ * This interface extends JpaRepository, providing CRUD operations and custom query methods for Transaction entities.
+ */
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    /**
+     * Finds a transaction by its owner ID.
+     * @param ownerId The ID of the owner of the transaction.
+     * @return An Optional containing the Transaction if found, or empty if not found.
+     */
+    Optional<Transaction> findTransactionByOwnerId(String ownerId);
+
+    /**
+     * Finds a transaction by its owner ID and transaction type.
+     * @param ownerId The ID of the owner of the transaction.
+     * @param transactionType The type of the transaction (e.g., EXPENSE, INCOME).
+     * @return An Optional containing the Transaction if found, or empty if not found.
+     */
+    Optional<Transaction> findTransactionByOwnerIdAndTransactionType(String ownerId, TransactionTypes transactionType);
+}
